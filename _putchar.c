710903c6b62fe5bb9cf8d8f8f9a26653aa3b6dc2
TeeMeeLehin <samemerald8@gmail.com>
2023-03-27 15:47:15 +0100

    made updates to printf and main.h files

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
 * _putchar - function to write string to terminal
 * @c: the character to be written
--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
 
 
 int _printf(const char *format, ...);
