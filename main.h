267beb0554d3ffb32e842f03b96f9e3612eded98
TeeMeeLehin <samemerald8@gmail.com>
2023-03-27 13:06:07 +0100

    added _printf file and update main.h

--- main.h
+++ main.h
@@ -3,5 +3,8 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-
+int _print_char(char c);
+int _print_string(const char *s);
+int _print_format(const char *format, va_list args);
+int _printf(const char *format, ...);
 #endif
