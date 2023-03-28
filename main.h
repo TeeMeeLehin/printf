78ed95fd032ec4c0f066c6980c8789209305872b
ReabetsweL <Reabetswellale@gmail.com>
2023-03-28 13:45:10 -0700

    task 7

--- main.h
+++ main.h
@@ -18,6 +18,7 @@ int _print_char(char c);
 int _print_number(int n);
 int _print_string(const char *s);
 int _print_format(const char *format, va_list args);
+int _print_binary(unsigned int n);
 
 
 #endif
